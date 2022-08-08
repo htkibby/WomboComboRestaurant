f3982d8f66e2f45018b61525beaba59c0598e584
Thomas Kibby <htkibby@gmail.com>
2022-08-08 18:20:02 -0500

    empty html js thing

--- scripts/tkscripts.js
+++ scripts/tkscripts.js
@@ -2,8 +2,8 @@ const bart = [
    {
       id: 1,
       table: 101,
-      drinks: ["mojito", "beer", "coke"],
-      food: ["steak", "chicken", "pot stickers"],
+      drinks: ["mojito", " beer", " coke"],
+      food: ["steak", " chicken", " pot stickers"],
       occupancy: 4,
       needsBooster: false,
       bill: 460,
@@ -13,8 +13,8 @@ const bart = [
    {
       id: 2,
       table: 102,
-      drinks: ["sprite", "coke zero", "orange juice"],
-      food: ["bread sticks", "ceasar salad", "chicken tenders", "large lobster"],
+      drinks: ["sprite", " coke zero", " orange juice"],
+      food: ["bread sticks", " ceasar salad", " chicken tenders", " large lobster"],
       occupancy: 3,
       needsBooster: true,
       bill: 265,
@@ -39,23 +39,27 @@ for (calc of bart) {
    calc.tip = calc.bill * .2
 }
 
-let htmlWaiter = 'Bart Tables<ul>'
+let htmlWaiter = '<p class="waiterTitle">Bart Tables</p><ul>';
+let htmlEmpty = `<ul>`;
 for (const waiter of bart) {
    console.log(waiter)
    if (waiter.occupied === true && waiter.needsBooster === true) {
       htmlWaiter += `<li><b>Table</b> ${waiter.table}</<li>`
-      htmlWaiter += `<li>drinks: ${waiter.drinks}</<li>`
-      htmlWaiter += `<li>food: ${waiter.food}</<li>`
-      htmlWaiter += `<li><b>Table Needs a Booster Seat!</b></<li>`
-      htmlWaiter += `<li>occupancy: ${waiter.occupancy}</<li><br></br>`
+      htmlWaiter += `<li>Drinks: ${waiter.drinks}</<li>`
+      htmlWaiter += `<li>Food: ${waiter.food}</<li>`
+      htmlWaiter += `<li>Bill: $${waiter.bill}</<li>`
+      htmlWaiter += `<li><b>TABLE NEEDS A BOOSTER SEAT!</b></<li>`
+      htmlWaiter += `<li>Occupancy: ${waiter.occupancy}</<li><br></br>`
    } else if (waiter.occupied === true) {
       htmlWaiter += `<li><b>Table</b> ${waiter.table}</<li>`
-      htmlWaiter += `<li>drinks: ${waiter.drinks}</<li>`
-      htmlWaiter += `<li>food: ${waiter.food}</<li>`
-      htmlWaiter += `<li>occupancy: ${waiter.occupancy}</<li><br></br>`
+      htmlWaiter += `<li>Drinks: ${waiter.drinks}</<li>`
+      htmlWaiter += `<li>Food: ${waiter.food}</<li>`
+      htmlWaiter += `<li>Bill: $${waiter.bill}</<li>`
+      htmlWaiter += `<li>Occupancy: ${waiter.occupancy}</<li><br></br>`
    }
 }
 htmlWaiter += '</ul>'
+htmlEmpty += `</ul>`
 
 document.getElementById('waitertk').innerHTML = htmlWaiter
 document.getElementById('waitersj').innerHTML = htmlWaiter
