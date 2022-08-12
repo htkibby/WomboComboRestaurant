39cc4d0275eeb77536fc8d5c64a7343b31b9d185
yogi <ychaudhuri5@gmail.com>
2022-08-11 21:08:56 -0500

    final

--- scripts/yogiscripts.js
+++ scripts/yogiscripts.js
@@ -50,14 +50,79 @@
  }
 
 
-
-
-
-
  ]
- console.log(fabina)
+let htmlCalc = ``
 
- let fabinaTip=0
+let sumBill = 0
+Fabina.forEach(element => {
+   sumBill += element.bill;
+})
+htmlCalc += `<p>fabina has earned Big Brother Restaurant <b>$${sumBill}</b>.`
+
+let sumTip = 0
+bart.forEach(element => {
+   sumTip += element.bill *.2;
+})
+htmlCalc += `<p>fabina has earned <b>$${sumTip}</b> in tips.</p>`
+
+for (calc of bart) {
+   calc.tip = calc.bill * .2
+//    htmlCalc += `Bartholomew has an estimated $${calc.tip} in tips for the night.`
+}
+
+let htmlWaiter = '<p class="waiterTitle">Bart Tables</p><ul>';
+let htmlEmpty = ``;
+for (const waiter of fabina) {
+   // console.log(waiter)
+   if (waiter.occupied === true && waiter.needsBooster === true) {
+      htmlWaiterFabina += `<li><b>Table</b> ${waiter.table}</li>`
+      htmlWaiterFabina += `<li>Drinks: ${waiter.drinks}</li>`
+      htmlWaiterFabina += `<li>Food: ${waiter.food}</li>`
+      htmlWaiterFabina+= `<li>Bill: $${waiter.bill}</li>`
+      htmlWaiterFabina += `<li><b>TABLE NEEDS A BOOSTER SEAT!</b></li>`
+      htmlWaiterFabina += `<li>Occupancy: ${waiter.occupancy}</li><br></br>`
+   } else if (waiter.occupied === true) {
+      htmlWaiterFabina += `<li><b>Table</b> ${waiter.table}</li>`
+      htmlWaiterFabina += `<li>Drinks: ${waiter.drinks}</li>`
+      htmlWaiterFabina += `<li>Food: ${waiter.food}</li>`
+      htmlWaiterFabina += `<li>Bill: $${waiter.bill}</li>`
+      htmlWaiterFabina += `<li>Occupancy: ${waiter.occupancy}</<li><br></br>`
+   } else if (waiter.bill === 0) { 
+      htmlEmpty += `<p class="waiterTitle"><b>Table ${waiter.table}</b></p>`
+      htmlEmpty += `<p><b>NEVER SERVE THIS GROUP OF PEOPLE AGAIN CAUSE THEY DID NOT PAY ME - SINCERELY BARTHOLOMEW</b></p><br></br>`
+   } else {
+      htmlEmpty += `<p class="waiterTitle"><b>Table ${waiter.table}</b></p>`
+      htmlEmpty += `<ul><li>Final Bill $${waiter.bill}</li>`
+      htmlEmpty += `<li>Final Tip $${calc.tip}</li></ul><br></br>`
+   }
+}
+htmlWaiter += '</ul>'
+// htmlEmpty += ``
+
+//document.getElementById('waitertk').innerHTML = htmlWaiter
+//document.getElementById('waitersj').innerHTML = htmlWaiter
+//document.getElementById('waiterec').innerHTML = htmlWaiter
+//document.getElementById('waiteryogi').innerHTML = htmlWaiter
+
+//document.getElementById('emptyTables1').innerHTML = htmlEmpty
+//document.getElementById('emptyTables2').innerHTML = htmlEmpty
+//document.getElementById('emptyTables3').innerHTML = htmlEmpty
+//document.getElementById('emptyTables4').innerHTML = htmlEmpty
+
+//document.getElementById('totals1').innerHTML = htmlCalc
+//document.getElementById('totals2').innerHTML = htmlCalc
+////document.getElementById('totals3').innerHTML = htmlCalc
+//document.getElementById('totals4').innerHTML = htmlCalc
+document.getElementById('waiteryogi').innerHTML=htmlWaiterFabina
+
+document.getElementById('emptyTables4').innerHTML=htmlWaiterFabina
+
+document.getElementById('emptyTables').innerHTML=htmlcalcFabina
+
+// 
+ //console.log(fabina)
+
+ /*let fabinaTip=0
  for(calc of fabina){
     fabinaTip+=calc.bill *.2
  }
