b0a7f18472277b1fc83b937a108bf85bb610acab
Thomas Kibby <htkibby@gmail.com>
2022-08-11 21:16:41 -0500

    updated

--- scripts/yogiscripts.js
+++ scripts/yogiscripts.js
@@ -119,38 +119,3 @@ document.getElementById('emptyTables4').innerHTML=htmlWaiterFabina
 
 document.getElementById('emptyTables').innerHTML=htmlcalcFabina
 
-// 
- //console.log(fabina)
-
- /*let fabinaTip=0
- for(calc of fabina){
-    fabinaTip+=calc.bill *.2
- }
- console.log(fabinaTip)
-
-
- let htmlWaiterFab = 'FabinaTables<ul>'
-for (const waiter of fabina) {
-   console.log(fabina)
-   if (waiter.occupied === true && waiter.needsBooster === true) {
-      htmlWaiterFab += `<li>Table ${waiter.table}</<li>`
-      htmlWaiterFab += `<li>drinks: ${waiter.drinks}</<li>`
-      htmlWaiterFab += `<li>food: ${waiter.food}</<li>`
-      htmlWaiterFab += `<li>Table Needs a Booster Seat!<li>`
-      htmlWaiterFab += `<li>occupancy: ${waiter.occupancy}</<li><br></br>`
-   } else if (waiter.occupied === true) {
-      htmlWaiterFab += `<li>Table ${waiter.table}</<li>`
-      htmlWaiterFab += `<li>drinks: ${waiter.drinks}</<li>`
-      htmlWaiterFab += `<li>food: ${waiter.food}</<li>`
-      htmlWaiterFab += `<li>occupancy: ${waiter.occupancy}</<li><br></br>`
-   } else {
-    htmltable += '<p class="waiterbook"> table $(waiter.table)</p> '
-   }
-
-}
-htmlWaiter += '</ul>'
-
-document.getElementById('waitertk').innerHTML = htmlWaiter
-document.getElementById('waitersj').innerHTML = htmlWaiter
-document.getElementById('waiterec').innerHTML = htmlWaiter
-document.getElementById('waiteryogi').innerHTML = htmlWaiter
