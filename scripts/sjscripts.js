5b0789b54ca4fc318ac2b1888eedcd4bde9bfc92
StevenAJohnsonJr <sajohnson187@gmail.com>
2022-08-11 19:44:50 -0500

    final Upload

--- scripts/sjscripts.js
+++ scripts/sjscripts.js
@@ -2,14 +2,15 @@ const pepper = [
     {
         id: 1,
         table: 003,
-        drinks: ["Crown and Coke", "Royle Flush", "Margarita Pitcher "],
+        drinks: ["Crown and Coke", " Royle Flush", " Margarita Pitcher "],
         food: [
-            "Chicken and Rice",
+            " Chicken and Rice",
             "Rib-Tips",
             "Sampler- Platter",
             "Veggies",
             "Meatloaf",
         ],
+        occupied: true,
         occupancy: 5,
         needsBooster: true,
         bill: 159,
@@ -21,6 +22,7 @@ const pepper = [
         table: 004,
         drinks: ["Lemonade", "Texas Tea", "Sweet Tea", "Lemonade Tea"],
         food: ["Chicken Strips", "Mac-n-Cheese", "Surf-n-Turf", "Pie"],
+        occupied: true,
         occupancy: 4,
         needsBooster: true,
         bill: 159,
@@ -32,6 +34,7 @@ const pepper = [
         table: 005,
         drinks: ["Sprite", "Coke", "Orange Soda"],
         food: ["Peperoni Pizza"],
+        occupied: true,
         occupancy: 4,
         needsBooster: false,
         bill: 340,
@@ -46,16 +49,43 @@ const pepper = [
             "Medium-Well Rib-eye Steak and Grilled Shrimp",
             "Well-Done Rib-eye Steak and Potatoe",
         ],
+        occupied: true,
         occupancy: 2,
         needsBooster: true,
         bill: 159,
         tip: "",
     },
 ];
+//console.log(pepper);
 
+let htmlWaiter = "Pepper Tables<ul>";
+for (const waiter of pepper) {
+    console.log(waiter)
+    if (waiter.occupied === true && waiter.needsBooster === true) {
+        htmlWaiter += `<li><b>Table</b> ${waiter.table}</<li>`;
+        htmlWaiter += `<li>drinks: ${waiter.drinks}</<li>`;
+        htmlWaiter += `<li>food: ${waiter.food}</<li>`;
+        htmlWaiter += `<li><b>Table Needs a Booster Seat!</b></<li>`;
+        htmlWaiter += `<li>occupancy: ${waiter.occupancy}</<li><br></br>`;
+    } else if (waiter.occupied === true) {
+        htmlWaiter += `<li><b>Table</b> ${waiter.table}</<li>`;
+        htmlWaiter += `<li>drinks: ${waiter.drinks}</<li>`;
+        htmlWaiter += `<li>food: ${waiter.food}</<li>`;
+        htmlWaiter += `<li>occupancy: ${waiter.occupancy}</<li><br></br>`;
+    }
+}
+htmlWaiter += "</ul>";
+/*for (const boosterSeat of pepper) {
+    if (boosterSeat === boosterSeat.needsBooster === true)
+        console.log(boosterSeat.needsBooster)
+}*/
 let pepperTip = 0;
 for (calc of pepper) {
     pepperTip += calc.bill * 2;
+    console.log(pepperTip);
 }
-console.log(pepperTip);
 
+/*document.getElementById('waitertk').innerHTML = htmlWaiter
+document.getElementById('waitersj').innerHTML = htmlWaiter
+document.getElementById('waiterec').innerHTML = htmlWaiter
+document.getElementById('waiteryogi').innerHTML = htmlWaiter*/
